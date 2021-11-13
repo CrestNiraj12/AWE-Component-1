590160d2d00dad8501e40abd57d0a6cc072eba36
CrestNiraj12 <crestniraj@gmail.com>
2021-11-13 21:09:50 +0545

    Added documentation

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
 
-set_include_path(get_include_path().PATH_SEPARATOR.'./'); //add current directory as to include path
+set_include_path(get_include_path() . PATH_SEPARATOR . './'); //add current directory as to include path
 
 spl_autoload_register(function ($class_name) {
-    $class_name=str_replace('\\', '/',  $class_name); //Issue on AET Eclipse Che on file paths
+    $class_name = str_replace('\\', '/',  $class_name); //Issue on AET Eclipse Che on file paths
     include $class_name . '.php';
 });
 
 
-$api_call=false;
+$api_call = false;
 
-if($_SERVER['REQUEST_METHOD'] == 'GET') {
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['delete'])) {
         \awe\JsonUtility::deleteProductWithId("products.json", $_GET['delete']);
     }
     if (isset($_GET['api'])) {
-        $api_call=$_GET['api'];
+        $api_call = $_GET['api'];
     }
 }
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['producttype'])) {
         $producttype = $_POST['producttype'];
     }
@@ -40,7 +40,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     \awe\JsonUtility::addNewProduct("products.json", $producttype, $fname, $sname, $title, $pages, $price);
 }
 
-if($api_call)   $products = new \awe\JsonProductWriter();
+if ($api_call)   $products = new \awe\JsonProductWriter();
 else            $products = new \awe\HtmlProductWriter();
 
 $productArray = \awe\JsonUtility::makeProductArray("products.json");
@@ -49,6 +49,3 @@ $products->setProducts($productArray);
 
 
 $products->write();
-
-
-
